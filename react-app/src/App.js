efaa187ec806965213b587742eeb5c4c9aedbc3d
Jamie Guerrero <jamie.n.guerrero@gmail.com>
2017-09-22 13:09:30 -0400

    added refs

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -5,17 +5,26 @@ class App extends React.Component {
     super();
     this.state = {a: ''}
   }
-  update(e){
-    this.setState({a: e.target.value})
+  update(){
+    this.setState({
+      a: this.refs.a.value,
+      b: this.refs.b.value
+    })
   }
   render(){
     return (
       <div>
         <input
+        ref="a"
         type="text"
         onChange={this.update.bind(this)}
         /> {this.state.a}
         <hr/>
+        <input
+        ref="b"
+        type="text"
+        onChange={this.update.bind(this)}
+        /> {this.state.b}
       </div>
     )
   }
