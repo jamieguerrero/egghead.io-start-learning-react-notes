e5242030af1ff4ec5a64dfd01e221c73f5f09100
Jamie Guerrero <jamie.n.guerrero@gmail.com>
2017-09-22 13:12:58 -0400

    made input component... doesn't work tho. need to fix setState

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import ReactDOM from 'react-dom'
 
 class App extends React.Component {
   constructor(){
@@ -7,15 +8,15 @@ class App extends React.Component {
   }
   update(){
     this.setState({
-      a: this.a.value,
+      a: ReactDOM.findDOMNode(this.a).value,
       b: this.b.value
     })
   }
   render(){
     return (
       <div>
-        <input
-        ref={node => this.a = node}
+        <Input
+        ref={component => this.a = component}
         type="text"
         onChange={this.update.bind(this)}
         /> {this.state.a}
@@ -30,16 +31,9 @@ class App extends React.Component {
   }
 }
 
-const Title = (props) => <h1>Title: {props.text}</h1>
-
-Title.propTypes = {
-  text(props, propName, component){
-    if(!(propName in props)){
-      return new Error(`missing ${propName}`)
-    }
-    if(props[propName].length < 6){
-      return new Error(`${propName} was too short`)
-    }
+class Input extends React.Component {
+  render(){
+    return <input type="text" onChange={this.props.update}/>
   }
 }
 
