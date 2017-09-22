08546de6223ca419569dc3f64aea793eaf817bdf
Jamie Guerrero <jamie.n.guerrero@gmail.com>
2017-09-22 14:09:52 -0400

    Finished lesson 14

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -4,27 +4,37 @@ import ReactDOM from 'react-dom'
 class App extends React.Component {
   constructor(){
     super();
-    this.state = {increasing: false}
+    this.state = {items: []}
   }
-  update(){
-    ReactDOM.render(<App val={this.props.val + 1} />, document.getElementById('root'))
+
+  componentWillMount(){
+    fetch ('http://swapi.co/api/people/?format=json')
+    .then( response => response.json() )
+    .then( ({results: items}) => this.setState({items}))
   }
-  componentWillReceiveProps(nextProps){
-    this.setState({increasing: nextProps.val > this.props.val})
-  }
-  // state is being changed. it is just not rendering. you can hit button 5 times to make it change
-  shouldComponentUpdate(nextProps,nextState){
-    return nextProps.val % 5 === 0;
+  filter(e){
+    this.setState({filter: e.target.value})
   }
   render(){
-    console.log(this.state.increasing)
-    return <button onClick={this.update.bind(this)}>{this.props.val}</button>
-
-  }
-  componentDidUpdate(prevProps, prevState) {
-    console.log(`prevProps: ${prevProps.val}`)
+    let items = this.state.items
+    if(this.state.filter){
+      items = items.filter( item =>
+        item.name.toLowerCase()
+        .includes(this.state.filter.toLowerCase()))
+    }
+    return (
+      <div>
+        <input type="text"
+        onChange={this.filter.bind(this)} />
+        {items.map(item =>
+          //key is needed on siblings, not on the component Person! :)
+          <Person key={item.name} person={item} />)}
+      </div>
+    )
   }
 }
+
+const Person = (props) => <h4>{props.person.name}</h4>
 
 App.defaultProps = {val: 0}
 
