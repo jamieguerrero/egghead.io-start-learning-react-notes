93ac710306536b4d394cc3948559cc6490cbbcfe
Jamie Guerrero <jamie.n.guerrero@gmail.com>
2017-09-22 12:25:54 -0400

    Starting on lesson 6

---
+++ react-app/src/App.js
@@ -0,0 +1,29 @@
+import React from 'react';
+
+class App extends React.Component {
+  constructor(){
+    //gives context
+    super();
+    this.state = {
+      txt: 'this is the state txt'
+    }
+  }
+
+  update( e ) {
+    this.setState({txt: e.target.value})
+  }
+
+  render(){
+    return (
+      <div>
+        <h1>{this.state.txt}</h1>
+        <Widget update={this.update.bind(this)} />
+      </div>
+    )
+  }
+}
+
+const Widget = (props) =>
+  <input type="text" onChange={props.update}/>
+
+export default App
