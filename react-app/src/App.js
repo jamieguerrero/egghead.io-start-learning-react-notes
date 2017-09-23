d0ae65bc69d3625bad35536eea5c8b0f79e64f57
Jamie Guerrero <jamie.n.guerrero@gmail.com>
2017-09-23 09:31:32 -0400

    Finished lesson 18

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,43 +1,45 @@
 import React from 'react';
 import './App.css';
 
+// if we remove childB while render is:
+// render() {
+//   let items = this.props.children.map(child => child)
+//   console.log(items)
+//   return null
+// }
+// it will throw an error!
+
 class App extends React.Component {
-  constructor(){
-    super();
-    this.state = {
-      input: '/* add your jsx here */',
-      output: '',
-      err: ''
-    }
-  }
-  update(e){
-    let code = e.target.value;
-    //transform using these presets and then send code
-    try {
-      this.setState({
-        output: window.Babel
-        .transform(code, {presets: ['es2015', 'react']})
-        .code
-      })
-    }
-    catch(err){
-      this.setState({err: err.message})
-    }
-  }
   render(){
     return (
-      <div>
-        <header>{this.state.err}</header>
-        <div className="container">
-          <textarea
-            onChange={this.update.bind(this)}
-            defaultValue={this.state.input}/>
-          <pre>
-            {this.state.output}
-          </pre>
-        </div>
-      </div>
+      <Parent>
+        <div className="childA"></div>
+        <div className="childB"></div>
+      </Parent>
     )
+  }
+}
+
+class Parent extends React.Component {
+  render() {
+    // let items = this.props.children.map(child => child)
+
+    // React.Children.map takes two arguments
+    // let items = React.Children
+    //   .map(this.props.children, child => child)
+
+    // Can also use forEach
+    // let items = React.Children
+    //   .forEach(this.props.children, child => console.log(child.props.className))
+
+    // in this case, we can handle adjacent siblings with forArray
+    // React.Children.toArray(this.props.children)
+
+    // this returns single child, and if more than one returns error
+    React.Children.only(this.props.children)
+
+    console.log(items)
+    return null
   }
 }
 
